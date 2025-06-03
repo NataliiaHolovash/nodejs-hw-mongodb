af3d384c450ce5d415b765597b8aeb1b5ce630a7
NataliiaHolovash <n.golovash@ukr.net>
2025-06-03 13:36:51 +0200

    server

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
-import authRouter from './routers/auth.js';
+
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -16,9 +16,7 @@ export const startServer = () => {
     app.use(express.json());
     app.use(cors());
     app.use(cookieParser());
-    app.use('/auth', authRouter);
-    app.use('/contacts', router);
-    app.use('/contacts/:id', router);
+    app.use('/', router);
     app.use(notFoundHandler);
     app.use(errorHandler);
 
