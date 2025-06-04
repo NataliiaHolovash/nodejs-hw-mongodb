91dea2a36212f37f49ac8a88bd6c2796750fedd3
NataliiaHolovash <n.golovash@ukr.net>
2025-06-04 13:05:42 +0200

    router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -18,12 +18,12 @@ const router = Router();
 
 
 router.use(authenticate);
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
-router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
-router.put('/contacts/:contactId', isValidId, validateBody(createContactSchema), ctrlWrapper(upsertContactController));
-router.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.get('/', ctrlWrapper(getContactsController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.put('/:contactId', isValidId, validateBody(createContactSchema), ctrlWrapper(upsertContactController));
+router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 
 
